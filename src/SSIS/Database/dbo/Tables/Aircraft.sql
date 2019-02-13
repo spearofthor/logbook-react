b8046928d84e92b85adee4352918408e3985a938
Shane Torgerson <storgerson@athenahealth.com>
2019-02-13 09:22:58 -0600

    WIP
    Adding Aircraft INGEST

---
+++ src/SSIS/Database/dbo/Tables/Aircraft.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Aircraft] (
+    [AircraftID]      VARCHAR (100) NULL,
+    [TypeCode]        VARCHAR (100) NULL,
+    [Year]            VARCHAR (100) NULL,
+    [Make]            VARCHAR (100) NULL,
+    [Model]           VARCHAR (100) NULL,
+    [Category]        VARCHAR (100) NULL,
+    [Class]           VARCHAR (100) NULL,
+    [GearType]        VARCHAR (100) NULL,
+    [EngineType]      VARCHAR (100) NULL,
+    [Complex]         VARCHAR (100) NULL,
+    [HighPerformance] VARCHAR (100) NULL,
+    [Pressurized]     VARCHAR (100) NULL
+);
+
